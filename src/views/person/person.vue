5f444073d1976dbbd417220e577948a900bcfd48
int*gq <@gaoq1118>
2022-01-13 15:07:43 +0800

    reset

---
+++ src/views/person/person.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="components-container">
+
+    <pan-thumb :image="image" />
+
+    <!-- <el-button type="primary" icon="el-icon-upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">
+      Change Avatar
+    </el-button> -->
+
+    <image-cropper
+      v-show="imagecropperShow"
+      :key="imagecropperKey"
+      :width="300"
+      :height="300"
+      url="https://httpbin.org/post"
+      lang-type="en"
+      @close="close"
+      @crop-upload-success="cropSuccess"
+    />
+
+    <!-- <pan-thumb :image="image" @click="imagecropperShow=true" />
+
+    <el-button type="primary" icon="el-icon-upload" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">
+        修改头像
+    </el-button>
+
+    <el-button type="primary" icon="user" style="position: absolute;bottom: 15px;margin-left: 40px;" @click="imagecropperShow=true">
+        <pan-thumb :image="image" />
+    </el-button> -->
+    <!--
+    <image-cropper
+      v-show="imagecropperShow"
+      :key="imagecropperKey"
+      :width="300"
+      :height="300"
+      url="https://httpbin.org/post"
+      lang-type="en"
+      @close="close"
+      @crop-upload-success="cropSuccess"
+    /> -->
+
+    <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%; margin-top:50px;">
+      <el-table-column align="center" label="ID" width="80">
+        <template slot-scope="{row}">
+          <span>{{ row.id }}</span>
+        </template>
+      </el-table-column>
+
+      <!-- <el-table-column width="180px" align="center" label="Date">
+        <template slot-scope="{row}">
+          <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="120px" align="center" label="Author">
+        <template slot-scope="{row}">
+          <span>{{ row.author }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="100px" label="Importance">
+        <template slot-scope="{row}">
+          <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
+        </template>
+      </el-table-column>
+
+      <el-table-column class-name="status-col" label="Status" width="110">
+        <template slot-scope="{row}">
+          <el-tag :type="row.status | statusFilter">
+            {{ row.status }}
+          </el-tag>
+        </template>
+      </el-table-column> -->
+
+      <el-table-column min-width="300px" label="Title">
+        <template slot-scope="{row}">
+          <template v-if="row.edit">
+            <el-input v-model="row.title" class="edit-input" size="small" />
+            <el-button
+              class="cancel-btn"
+              size="small"
+              icon="el-icon-refresh"
+              type="warning"
+              @click="cancelEdit(row)"
+            >
+              cancel
+            </el-button>
+          </template>
+          <span v-else>{{ row.title }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="Actions" width="120">
+        <template slot-scope="{row}">
+          <el-button
+            v-if="row.edit"
+            type="success"
+            size="small"
+            icon="el-icon-circle-check-outline"
+            @click="confirmEdit(row)"
+          >
+            Ok
+          </el-button>
+          <el-button
+            v-else
+            type="primary"
+            size="small"
+            icon="el-icon-edit"
+            @click="row.edit=!row.edit"
+          >
+            Edit
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
+import { fetchList } from '@/api/article'
+
+export default {
+  name: 'AvatarUploadDemo',
+  components: { ImageCropper, PanThumb },
+
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+
+  data() {
+    return {
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      //   image: '@/images/默认头像.jpg'
+      //   image: 'D:\研究生课程\高级软工\vue-element-admin\images\默认头像.jpg'
+      image: 'https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191',
+
+      list: null,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 10
+      }
+    }
+  },
+
+  created() {
+    this.getList()
+  },
+
+  methods: {
+    cropSuccess(resData) {
+      this.imagecropperShow = false
+      this.imagecropperKey = this.imagecropperKey + 1
+      this.image = resData.files.avatar
+    },
+    close() {
+      this.imagecropperShow = false
+    },
+
+    async getList() {
+      this.listLoading = true
+      const { data } = await fetchList(this.listQuery)
+      const items = data.items
+      this.list = items.map(v => {
+        this.$set(v, 'edit', false) // https://vuejs.org/v2/guide/reactivity.html
+        v.originalTitle = v.title //  will be used when user click the cancel botton
+        return v
+      })
+      this.listLoading = false
+    },
+    cancelEdit(row) {
+      row.title = row.originalTitle
+      row.edit = false
+      this.$message({
+        message: 'The title has been restored to the original value',
+        type: 'warning'
+      })
+    },
+    confirmEdit(row) {
+      row.edit = false
+      row.originalTitle = row.title
+      this.$message({
+        message: 'The title has been edited',
+        type: 'success'
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .avatar{
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
+
+  .edit-input {
+  padding-right: 100px;
+}
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+</style>
+
---
+++ src/views/search/search.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="components-container">
+    <el-drag-select v-model="value" style="width:500px;" multiple placeholder="请选择">
+      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+    </el-drag-select>
+
+    <div style="margin-top:30px;">
+      <el-tag v-for="item of value" :key="item" style="margin-right:15px;">
+        {{ item }}
+      </el-tag>
+    </div>
+
+        <el-row :gutter="20">
+      <!-- 行内消息1 -->
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div>
+            <el-col :span="20" class="text-center ">
+
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag1
+                </router-link>
+              </el-col>
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag2
+                </router-link>
+              </el-col>
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag3
+                </router-link>
+              </el-col>
+            </el-col>
+
+            <el-col :span="4" class="text-center" style="border-left: 1px solid black">
+              <p>Tag</p>
+            </el-col>
+          </div>
+
+          <div style="margin-top:50px; border-top:2px solid black; ">
+            消息1
+          </div>
+
+          <div style="margin-top:10px; border-top:2px solid black; height: 150px;">
+            消息内容
+          </div>
+
+          <div style="margin-top:10px; border-top:2px solid black;">
+            <el-col :span="12" style="text-align:left;">
+              发布者
+            </el-col>
+
+            <el-col :span="12" style="text-align:right;">
+              发布时间
+            </el-col>
+          </div>
+        </el-card>
+      </el-col>
+      <!-- 行内消息1结束 -->
+
+      <!-- 行内消息2 -->
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div>
+            <el-col :span="20" class="text-center ">
+
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag1
+                </router-link>
+              </el-col>
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag2
+                </router-link>
+              </el-col>
+              <el-col :span="6" class="text-center">
+                <router-link class="pan-btn tiffany-btn" to="/example/create">
+                  tag3
+                </router-link>
+              </el-col>
+            </el-col>
+
+            <el-col :span="4" class="text-center" style="border-left: 1px solid black">
+              <p>Tag</p>
+            </el-col>
+          </div>
+
+          <div style="margin-top:50px; border-top:2px solid black; ">
+            消息2
+          </div>
+
+          <div style="margin-top:10px; border-top:2px solid black; height: 150px;">
+            消息内容
+          </div>
+
+          <div style="margin-top:10px; border-top:2px solid black;">
+            <el-col :span="12" style="text-align:left;">
+              发布者
+            </el-col>
+
+            <el-col :span="12" style="text-align:right;">
+              发布时间
+            </el-col>
+          </div>
+        </el-card>
+      </el-col>
+      <!-- 行内消息2结束 -->
+
+    </el-row>
+  </div>
+</template>
+
+<script>
+import ElDragSelect from '@/components/DragSelect' // base on element-ui
+
+export default {
+  name: 'DragSelectDemo',
+  components: { ElDragSelect },
+  data() {
+    return {
+      value: ['Apple', 'Banana', 'Orange'],
+      options: [{
+        value: 'Apple',
+        label: 'Apple'
+      }, {
+        value: 'Banana',
+        label: 'Banana'
+      }, {
+        value: 'Orange',
+        label: 'Orange'
+      }, {
+        value: 'Pear',
+        label: 'Pear'
+      }, {
+        value: 'Strawberry',
+        label: 'Strawberry'
+      }]
+    }
+  }
+}
+</script>
